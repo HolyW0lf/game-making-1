98abd3d81f585b5563e87a7a13aa4848a3116237
HolyW0lf <gvking1234@gmail.com>
2024-01-13 16:18:56 +0100

    nahh

--- Assets/New Folder/Scrips/gun.cs
+++ Assets/New Folder/Scrips/gun.cs
@@ -2,26 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Gun : MonoBehaviour {
+public class Gun : MonoBehaviour{
 
-    public Transform bulletSpawn;
-    public Rigidbody bullet;
-    public float bulletSpeed;
-
-
-    // Update is called once per frame
-    void Update() {
-        if (Input.GetButtonDown("Fire1"))
-        {
-            Rigidbody bulletrigidbody;
-            bulletrigidbody = Instantiate(bullet, bulletSpawn.position, bulletSpawn.rotation)as Rigidbody;
-            bulletrigidbody.AddForce(bulletSpawn.forward * bulletSpeed);
-
-
-
-
-
-
-        }
-    }
+    [SerializeField] GunData gunData;
 }
